846ca353c2bae266f59ddc9d7b4cfb630069c3af
Mustafa Nomair <nomair@usc.edu>
2025-04-21 23:41:35 -0700

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,15 @@ export const metadata = {
   manifest: "/manifest.json",
   themeColor: "#6d28d9",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0",
-    generator: 'v0.dev'
+  generator: 'v0.dev',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png', 
+  }
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
